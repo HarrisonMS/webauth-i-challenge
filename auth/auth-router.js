5b78c1d302bd7e21cb1ab16a2af05759cb43c999
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-12-17 15:24:33 -0800

    logout functionality

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -33,7 +33,7 @@ router.post("/login", (req, res) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         // in here with .compare()
         // change the users-model findBy() to return the password as well
-        res.status(200).json({ message: `How you doin ${user.username}` });
+        res.status(200).json({ message: `${user.username}, welcome to the website` });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
